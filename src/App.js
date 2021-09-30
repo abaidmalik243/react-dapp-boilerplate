6e267e7e2e0d199a58059c84d3fbc1e9f438273c
Abaidullah <abaidmalik243@gmail.com>
2021-09-30 17:50:40 +0500

    integration of metamask with app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMetaMask } from 'metamask-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const { status, connect, account } = useMetaMask();
+
+  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+
+  if (status === "unavailable") return <div>MetaMask not available :(</div>
+
+  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+
+  if (status === "connecting") return <div>Connecting...</div>
+
+  if (status === "connected") return <div>Connected account: {account}</div>
+
+  return 'Something went wrong on MetaMask Integration';
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
